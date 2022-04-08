414b2563e977c359b2cf64020e7e85164c3ce14e
Andrey Ignatev <ignatevandrey997@mail.ru>
2022-04-08 09:03:34 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './App'
+import './fonts.scss'
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<App/>, domContainer);
